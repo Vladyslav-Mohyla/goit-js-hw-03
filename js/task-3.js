f5f60058908b111368d5c3596fccd8f48624d967
Vladyslav Mohyla <vladislav.mogila@gmail.com>
2025-11-09 19:22:45 +0200

    - Added slugify function for creating URL-friendly strings - Added makeArray function to merge arrays with max length limit - Created filterArray function to filter numbers greater than a given value

--- js/task-3.js
+++ js/task-3.js
@@ -1,13 +1,17 @@
-function getElementWidth(content, padding, border) {
-    content = parseFloat(content);
-    padding = parseFloat(padding);
-    border = parseFloat(border);
-    const totalWidth = content + (padding + border) * 2;
+function filterArray(numbers, value) {
+    let filteredNumbers = [];
+    for (let i = 0; i < numbers.length; i += 1) {
+        const number = numbers[i];
+        if (number > value) {
+            filteredNumbers.push(number);
+        }
+    }
     
-    return totalWidth;
+    return filteredNumbers;
 }
-
-
-console.log(getElementWidth("50px", "8px", "4px")); // 74
-console.log(getElementWidth("60px", "12px", "8.5px")); // 101
-console.log(getElementWidth("200px", "0px", "0px")); // 200
+    
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76
